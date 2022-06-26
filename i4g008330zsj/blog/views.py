867c752e8340c76e6addcb11d9d9ce72311a0b7b
kay-four <88488188+kay-four@users.noreply.github.com>
2022-06-26 20:06:03 +0300

    added import include

--- i4g008330zsj/blog/views.py
+++ i4g008330zsj/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DetailView,UpdateView
+from django.views.generic import ListView, CreateView, DetailView,UpdateView, DeleteView
 
 from .models import Post 
 
@@ -20,7 +20,7 @@ class PostUpdateView(UpdateView):
     fields = "__all__"
     success_url = reverse_lazy("blog:all")
 
-class PostDeleteView(UpdateView):
+class PostDeleteView(DeleteView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:all")
